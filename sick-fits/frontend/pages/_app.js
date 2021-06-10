b931b6e89e4d5ad1f7c3b40849b1e96cd58f3122
Sam Hollyer <rsamhollyer@gmail.com>
2021-06-10 10:34:50 -0500

    add nprogress bar

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,6 +1,19 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done();
+});
+Router.events.on('routeChangeError', () => {
+  NProgress.done();
+});
 
 export default function MyApp({ Component, pageProps }) {
   return (
