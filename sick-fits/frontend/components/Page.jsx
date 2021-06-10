7ed528de9349dcd5fc6b706c6dc136303c3720ea
Sam Hollyer <rsamhollyer@gmail.com>
2021-06-10 10:09:53 -0500

    add globalt styles

--- sick-fits/frontend/components/Page.jsx
+++ sick-fits/frontend/components/Page.jsx
@@ -1,17 +1,75 @@
 import PropTypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
 
 function Page({ children }) {
   return (
     <div>
+      <GlobalStyles />
       <Header />
-      <h2>I am the page component</h2>
-      {children}
+      <InnerStyles>{children}</InnerStyles>
     </div>
   );
 }
 
 export default Page;
+
+const GlobalStyles = createGlobalStyle`
+@font-face {
+font-family: 'radnika_next';
+src:url("/.static/radnikanext-medium-webfont.woff2")
+  format('woff2');
+  font-weight:normal;
+  font-style:normal
+}
+:root{
+  --red:#ff0000;
+  --black:#393939;
+  --grey:#3A3A3;
+  --gray: var(--grey);
+  --lightgrey:(#e1e1e1);
+  --lightgray:var(--lightgrey);
+  --offwhite:#ededed;
+  --maxWidth  :1000px;
+  --bs:0 12px 24px 0 rgba(0, 0, 0, 0.086)
+}
+
+*,*::before,*::after{
+  box-sizing:inherit;
+}
+
+html{
+  box-sizing:border-box;
+  padding:0;
+  margin: 0;
+  font-size:1.5rem;
+  line-height:2;
+}
+
+body{
+  font-family:'radnika_next',--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+}
+
+a{
+  text-decoration:none;
+  color:var(--black)
+}
+
+a:hover{
+  text-decoration:underline;
+}
+
+button{
+  font-family:'radnika_next',--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 Page.propTypes = {
   children: PropTypes.any,
