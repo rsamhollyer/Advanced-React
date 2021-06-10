f0c4d600b63d03731e1fee2b7b4166ebb65f4ea7
Sam Hollyer <rsamhollyer@gmail.com>
2021-06-10 16:17:03 -0500

    This is a lazy push

--- sick-fits/backend/schemas/ProductImage.ts
+++ sick-fits/backend/schemas/ProductImage.ts
@@ -1,7 +1,7 @@
 import { text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 import { cloudinaryImage } from '@keystone-next/cloudinary';
-import 'dotenv/config';
+import 'dotenv/config ';
 
 export const cloudinary = {
   cloudName: process.env.CLOUDINARY_CLOUD_NAME,
