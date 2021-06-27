dd5bc1e0409402f9c7c71bac7603e15a2b00587d
Sam Hollyer <rsamhollyer@gmail.com>
2021-06-27 11:37:22 -0500

    add pagination

--- sick-fits/frontend/pages/products.jsx
+++ sick-fits/frontend/pages/products.jsx
@@ -1,9 +1,12 @@
+import Pagination from '../components/Pagination';
 import Products from '../components/Products';
 
 export default function ProductsPage() {
   return (
     <div>
+      <Pagination page={5} />
       <Products />
+      <Pagination page={4} />
     </div>
   );
 }
