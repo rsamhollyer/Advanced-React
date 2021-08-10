72a8efe5cdb0b6e851ea391e7b17fa83840dd9d6
Sam Hollyer <rsamhollyer@gmail.com>
2021-08-10 04:09:30 -0500

    add remove cart button

--- sick-fits/frontend/components/Cart.jsx
+++ sick-fits/frontend/components/Cart.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import formatMoney from '../lib/formatMoney';
 import { useCart } from '../lib/hooks/cartState';
+import RemoveFromCart from './RemoveFromCart';
 import CartStyles from './styles/CartStyles';
 import CloseButton from './styles/CloseButton';
 import Supreme from './styles/Supreme';
@@ -27,6 +28,7 @@ function CartItem({ cartItem }) {
           </em>
         </p>
       </div>
+      <RemoveFromCart id={cartItem.id} />
     </CartItemStyles>
   );
 }
---
+++ sick-fits/frontend/components/RemoveFromCart.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+
+const REMOVE_FROM_CART_MUTATION = gql`
+  mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
+    deleteCartItem(id: $id) {
+      id
+    }
+  }
+`;
+
+export default function RemoveFromCart({ id }) {
+  const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
+    variables: { id },
+  });
+
+  return (
+    <BigButton
+      onClick={removeFromCart}
+      disabled={loading}
+      type="button"
+      title="Remove from Cart"
+    >
+      &times;
+    </BigButton>
+  );
+}
+
+const BigButton = styled.button`
+  font-size: 3rem;
+  background: none;
+  border: 0;
+
+  &:hover {
+    color: red;
+    cursor: pointer;
+  }
+`;
